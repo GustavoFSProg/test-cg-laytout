599086228515f8f4fef9c05474e4c8f4bc5f3293
Gustavo <gustavo.prog40@gmail.com>
2022-11-24 18:48:39 -0300

    first layout

---
+++ src/App.tsx
@@ -0,0 +1,132 @@
+import { Container, Input, H2, Text, Button, ComboBox } from './styled.app'
+
+function App() {
+  return (
+    <Container>
+      <H2>Create a School Educator Account</H2>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '100vw',
+          flexDirection: 'row',
+        }}
+      >
+        <Input type="email" placeholder="Email" />{' '}
+        <span style={{ marginLeft: '10px', fontSize: '20px' }}>*</span>
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '100vw',
+          flexDirection: 'row',
+        }}
+      >
+        <Input type="password" placeholder="Password" />
+        <span style={{ marginLeft: '10px', fontSize: '20px' }}>*</span>
+      </div>
+
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '100vw',
+          flexDirection: 'row',
+        }}
+      >
+        <Input type="password" placeholder="Confirm Password" />
+        <span style={{ marginLeft: '10px', fontSize: '20px' }}>*</span>
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '100vw',
+          flexDirection: 'row',
+        }}
+      >
+        <ComboBox>
+          <option>Hours Fit Title</option>
+          <option>Contry State</option>
+          <option>City State</option>
+        </ComboBox>
+        <span style={{ marginLeft: '10px', fontSize: '20px' }}>*</span>
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '100vw',
+          flexDirection: 'row',
+        }}
+      >
+        <Input type="text" placeholder="First Name" />
+        <span style={{ marginLeft: '10px', fontSize: '20px' }}>*</span>
+      </div>
+
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '100vw',
+          flexDirection: 'row',
+        }}
+      >
+        <Input type="text" placeholder="Last Name" />
+        <span style={{ marginLeft: '10px', fontSize: '20px' }}>*</span>
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '100vw',
+          flexDirection: 'row',
+        }}
+      >
+        <Input type="text" placeholder="Birthdate" />
+
+        <span style={{ marginLeft: '10px', fontSize: '20px' }}>*</span>
+      </div>
+
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '100vw',
+          flexDirection: 'row',
+        }}
+      >
+        <ComboBox>
+          <option>Role</option>
+          <option>Contry State</option>
+          <option>City State</option>
+          <br />
+        </ComboBox>
+        <span style={{ marginLeft: '10px', fontSize: '20px' }}>*</span>
+      </div>
+
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <input
+          type="checkbox"
+          style={{ minWidth: '17px', minHeight: '17px', marginRight: '7px' }}
+        />
+        Classroom Only? Single to Others
+      </div>
+      <Text>By Singning up you agree to our user agrements! And Privacy Police.</Text>
+      <Button type="button">Signin Up</Button>
+      <span>Already have an account?</span>
+      <a href="#">Sign in!</a>
+    </Container>
+  )
+}
+
+export default App
---
+++ src/styled.app.ts
@@ -0,0 +1,61 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  background: lightpink;
+  width: 100vw;
+  height: 100vh;
+`
+
+export const ComboBox = styled.select`
+  display: flex;
+  width: 31%;
+  height: 2.2rem;
+  border-radius: 8px;
+  padding-left: 10px;
+
+  @media screen and (max-width: 800px) {
+    width: 84%;
+  }
+`
+
+export const Text = styled.div`
+  @media screen and (max-width: 800px) {
+    width: 85%;
+  }
+`
+
+export const H2 = styled.h2`
+  @media screen and (max-width: 800px) {
+    font-size: 16px;
+  }
+`
+
+export const Input = styled.input`
+  display: flex;
+  width: 30%;
+  height: 2rem;
+  border-radius: 8px;
+  padding-left: 10px;
+
+  @media screen and (max-width: 800px) {
+    width: 80%;
+  }
+`
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 30%;
+  height: 2.3rem;
+  border-radius: 8px;
+  padding-left: 10px;
+
+  @media screen and (max-width: 800px) {
+    width: 80%;
+  }
+`
