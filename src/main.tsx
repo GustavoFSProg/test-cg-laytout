491bf2d4877ecf125edc28e35a141b137cfa9848
Gustavo <gustavo.prog40@gmail.com>
2022-11-26 10:22:10 -0300

    terminando

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Routing from './routes'
-import './index.css'
+// import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
